c1857001ade261afeb007aaace1a860c901846d7
Igor Nascimento Costa <igorncosta14@gmail.com>
2025-08-01 14:37:00 -0300

    chore: improve validation error messages

--- src/app/employee/dtos/UpdateEmployeeParams.dto.ts
+++ src/app/employee/dtos/UpdateEmployeeParams.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsUUID } from "class-validator";
 
 export class UpdateEmployeeParamsDto {
-    @IsNotEmpty()
-    @IsUUID()
+    @IsNotEmpty({ message: 'The "id" parameter is required in the route params' })
+    @IsUUID('4', { message: 'The "id" parameter in the route params must be a valid UUID' })
     id: string;
 }
